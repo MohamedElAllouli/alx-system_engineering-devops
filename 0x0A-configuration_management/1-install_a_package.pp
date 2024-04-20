e532de49a0e0085f4022dd960abc1c1d1917e4ea
MohamedElAllouli <alloulialloulimohamed2023@gamil.com>
2024-04-19 17:05:00 -0700

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-#!/usr/bin/pup
-# Install especific version of flask
+# install puppet-lint -v 2.5.0
 
-
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
