e807e05b6fb3a2a99ceae4ee79a4dcec838303f7
MohamedElAllouli <alloulialloulimohamed2023@gamil.com>
2024-04-19 17:42:57 -0700

    task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # Installs a package
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin'],
+  environment => ['PATH=/usr/bin'],
+  unless      => '/usr/bin/flask --version | grep "2.1.0"',
 }
