5254ee41291e8a45d50e5258f2c749725c89c3e6
MohamedElAllouli <alloulialloulimohamed2023@gamil.com>
2024-06-04 06:46:11 -0700

    task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,9 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Google Chrome Version 125.0.6422.142"}
+    headers = {"User-Agent": "Chrome/125.0.0.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 300:
+    if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
